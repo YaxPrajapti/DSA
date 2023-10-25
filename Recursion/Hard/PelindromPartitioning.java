3ee0f072535c55d87f695a9f8a57868d185d2a5b
Yax Prajapati <yaxprjpt@gmail.com>
2023-10-25 21:00:22 +0530

    Completed palindrom partitioning

--- Recursion/Hard/PelindromPartitioning.java
+++ Recursion/Hard/PelindromPartitioning.java
@@ -1,12 +1,42 @@
 package Recursion.Hard;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PelindromPartitioning {
     public static void main(String[] args) {
-        
+        String s = "aab"; 
+        List<List<String>> ans = partition(s); 
+        System.out.println(ans);
     }
     public static List<List<String>> partition(String s){
-        
+        List<List<String>> ans = new ArrayList<>(); 
+        List<String> path = new ArrayList<>(); 
+        int index = 0; 
+        int n = s.length(); 
+        generate(index, s, path, ans,n); 
+        return ans; 
     }
+    private static void generate(int index, String s, List<String> path, List<List<String>> ans,int n){
+        if(index == n){
+            ans.add(new ArrayList<>(path)); 
+            return; 
+        }
+        for(int i = index; i < n; i++){
+            if(isPalindrom(s, index, i)){
+                path.add(s.substring(index, i+1)); 
+                generate(i+1, s, path, ans, n);
+                path.remove(path.size()-1); 
+            }
+        }
+    }
+    private static boolean isPalindrom(String s, int start, int end){
+        while(start <= end){
+            if(s.charAt(start++) != s.charAt(end--)){
+                return false; 
+            }
+        }
+        return true; 
+    }
+    
 }
